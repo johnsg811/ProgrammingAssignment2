4c0c8e4d89836a02ed8a92ac2c6910ff43e1788a
Johns George <johnsg811@gmail.com>
2019-12-27 00:23:27 +0000

    commited

--- cachematrix.R
+++ cachematrix.R
@@ -13,7 +13,7 @@ makeCacheMatrix <- function( m = matrix() ) {
     }
 
     get <- function() {
-    	m
+        m
     }
 
     setInverse <- function(inverse) {
@@ -50,17 +50,4 @@ cacheSolve <- function(x, ...) {
     x$setInverse(m)
 
     m
-=======
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
->>>>>>> 7f657dd22ac20d22698c53b23f0057e1a12c09b7
 }
